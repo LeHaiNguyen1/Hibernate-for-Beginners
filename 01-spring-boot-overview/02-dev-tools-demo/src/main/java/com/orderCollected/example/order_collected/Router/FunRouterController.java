c2676f8d8373b08d6ec6baa7ca8f2c9664ba1e70
nguyen_le_hai <nguyen_le_hai@management-partners.co.jp>
2023-07-20 15:28:04 +0700

    Update demo filter new

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/orderCollected/example/order_collected/Router/FunRouterController.java
@@ -0,0 +1,31 @@
+package com.orderCollected.example.order_collected.Router;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+//class is marked as a RESTful controller using @RestController.
+@RestController
+public class FunRouterController {
+
+    // expose "/" that return "Hello word"
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // expost a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expost a new endpoint for "workout"
+
+    @GetMapping("/forturne")
+    public String getDailyForturne() {
+        return "Today is your lucky day.";
+    }
+}
