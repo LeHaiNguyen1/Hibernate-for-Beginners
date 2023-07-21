db4f41a80c3701637c0d3ccda9b4699c1a516cb2
nguyen_le_hai <nguyen_le_hai@management-partners.co.jp>
2023-07-21 16:58:19 +0700

    Update 2 section

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/orderCollected/example/order_collected/OrderCollectedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderCollected.example.order_collected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderCollectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/orderCollected/example/order_collected/OrderCollectedApplication.java
@@ -0,0 +1,13 @@
+package com.orderCollected.example.order_collected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCollectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderCollectedApplication.class, args);
+	}
+
+}
